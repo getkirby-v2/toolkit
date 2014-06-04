1c5df56885ceaa2edeae3a1b0b4eb29a88ee21d8
Bastian Allgeier <mail@bastianallgeier.com>
2014-06-04 17:26:33 +0200

    switched off keepTags option for the html helpers

--- helpers.php
+++ helpers.php
@@ -127,7 +127,7 @@ function attr($name, $value = null) {
  * @param string $text unencoded text
  * @return string
  */
-function html($text, $keepTags = true) {
+function html($text, $keepTags = false) {
   return html::encode($text, $keepTags);
 }
 
@@ -136,7 +136,7 @@ function html($text, $keepTags = true) {
  *
  * @see html()
  */
-function h($text, $keepTags = true) {
+function h($text, $keepTags = false) {
   return html::encode($text, $keepTags);
 }
 
