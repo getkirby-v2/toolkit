7a017e4fd6ff085d33759c27e8d06a2e7b6ce45d
Bastian Allgeier <mail@bastianallgeier.com>
2014-10-03 12:49:50 +0200

    new dir::wasModifiedAfter() method

--- lib/dir.php
+++ lib/dir.php
@@ -2,17 +2,17 @@
 
 /**
  * Dir
- * 
+ *
  * Low level directory handling utilities
- * 
- * @package   Kirby Toolkit 
+ *
+ * @package   Kirby Toolkit
  * @author    Bastian Allgeier <bastian@getkirby.com>
  * @link      http://getkirby.com
  * @copyright Bastian Allgeier
  * @license   http://www.opensource.org/licenses/mit-license.php MIT License
  */
 class Dir {
-  
+
   static public $defaults = array(
     'permissions' => 0755,
     'ignore'      => array('.', '..', '.DS_Store', '.gitignore', '.git', '.svn', '.htaccess')
@@ -20,15 +20,15 @@ class Dir {
 
   /**
    * Creates a new directory
-   * 
+   *
    * <code>
-   * 
+   *
    * $create = dir::make('/app/test/new-directory');
-   * 
+   *
    * if($create) echo 'the directory has been created';
-   * 
+   *
    * </code>
-   * 
+   *
    * @param   string  $dir The path for the new directory
    * @return  boolean True: the dir has been created, false: creating failed
    */
@@ -37,16 +37,16 @@ class Dir {
   }
 
   /**
-   * Reads all files from a directory and returns them as an array. 
-   * It skips unwanted invisible stuff. 
-   * 
+   * Reads all files from a directory and returns them as an array.
+   * It skips unwanted invisible stuff.
+   *
    * <code>
-   * 
+   *
    * $files = dir::read('mydirectory');
    * // returns array('file-1.txt', 'file-2.txt', 'file-3.txt', etc...);
-   * 
+   *
    * </code>
-   * 
+   *
    * @param   string  $dir The path of directory
    * @param   array   $ignore Optional array with filenames, which should be ignored
    * @return  mixed   An array of filenames or false
@@ -61,17 +61,17 @@ class Dir {
    * Moves a directory to a new location
    *
    * <code>
-   * 
+   *
    * $move = dir::move('mydirectory', 'mynewdirectory');
-   * 
+   *
    * if($move) echo 'the directory has been moved to mynewdirectory';
-   * 
+   *
    * </code>
-   * 
+   *
    * @param   string  $old The current path of the directory
    * @param   string  $new The desired path where the dir should be moved to
    * @return  boolean True: the directory has been moved, false: moving failed
-   */  
+   */
   static public function move($old, $new) {
     if(!is_dir($old)) return false;
     return @rename($old, $new);
@@ -79,19 +79,19 @@ class Dir {
 
   /**
    * Deletes a directory
-   * 
+   *
    * <code>
-   * 
+   *
    * $remove = dir::remove('mydirectory');
-   * 
+   *
    * if($remove) echo 'the directory has been removed';
-   * 
+   *
    * </code>
-   * 
+   *
    * @param   string   $dir The path of the directory
-   * @param   boolean  $keep If set to true, the directory will flushed but not removed. 
+   * @param   boolean  $keep If set to true, the directory will flushed but not removed.
    * @return  boolean  True: the directory has been removed, false: removing failed
-   */  
+   */
   static public function remove($dir, $keep = false) {
     if(!is_dir($dir)) return false;
 
@@ -102,20 +102,20 @@ class Dir {
 
   /**
    * Flushes a directory
-   * 
+   *
    * @param   string   $dir The path of the directory
    * @return  boolean  True: the directory has been flushed, false: flushing failed
-   */  
+   */
   static public function clean($dir) {
     return static::remove($dir, true);
   }
 
   /**
    * Gets the size of the directory and all subfolders and files
-   * 
+   *
    * @param   string $dir The path of the directory
-   * @return  mixed  
-   */  
+   * @return  mixed
+   */
   static public function size($dir) {
 
     if(!file_exists($dir)) return false;
@@ -128,23 +128,23 @@ class Dir {
 
   /**
    * Returns a nicely formatted size of all the contents of the folder
-   * 
+   *
    * @param string $dir The path of the directory
    * @param boolean $recursive
    * @return mixed
    */
   static public function niceSize($dir) {
     return static::size($dir);
-  } 
+  }
 
   /**
-   * Recursively check when the dir and all 
-   * subfolders have been modified for the last time. 
-   * 
+   * Recursively check when the dir and all
+   * subfolders have been modified for the last time.
+   *
    * @param   string   $dir The path of the directory
    * @param   string   $format
-   * @return  int  
-   */  
+   * @return  int
+   */
   static public function modified($dir, $format = null) {
     // It's easier to handle this with the Folder class
     $object = new Folder($dir);
@@ -152,8 +152,32 @@ class Dir {
   }
 
   /**
+   * Checks if the directory or any subdirectory has been
+   * modified after the given timestamp
+   *
+   * @param string $dir
+   * @param int $time
+   * @return boolean
+   */
+  static public function wasModifiedAfter($dir, $time) {
+
+    if(filemtime($dir) > $time) return true;
+
+    $content = dir::read($dir);
+
+    foreach($content as $item) {
+      $subdir = $dir . DS . $item;
+      if(filemtime($subdir) > $time) return true;
+      if(is_dir($subdir) and dir::wasModifiedAfter($subdir, $time)) return true;
+    }
+
+    return false;
+
+  }
+
+  /**
    * Checks if the dir is writable
-   * 
+   *
    * @param string $dir
    * @return boolean
    */
@@ -163,7 +187,7 @@ class Dir {
 
   /**
    * Checks if the dir is readable
-   * 
+   *
    * @param string $dir
    * @return boolean
    */
@@ -173,7 +197,7 @@ class Dir {
 
   /**
    * Copy a file, or recursively copy a folder and its contents
-   * 
+   *
    * @param string $dir Source path
    * @param string $to Destination path
    */
@@ -181,6 +205,6 @@ class Dir {
     // It's easier to handle this with the Folder class
     $object = new Folder($dir);
     return $object->copy($to);
-  }  
+  }
 
 }
