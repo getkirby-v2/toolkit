0ba93d37936e1b752fbfebaa0d0586f39323fc10
Bastian Allgeier <mail@bastianallgeier.com>
2014-12-13 00:37:41 +0100

    Additional Tests for the server class

--- test/ServerTest.php
+++ test/ServerTest.php
@@ -19,6 +19,12 @@ class ServerTest extends PHPUnit_Framework_TestCase {
     $this->assertEquals('alertxss', server::get('HTTP_HOST'));
     $this->assertEquals('alertxss', server::get('SERVER_NAME'));
 
+    $_SERVER['HTTP_HOST'] = '127.0.0.1';
+    $_SERVER['SERVER_NAME'] = '127.0.0.1';
+
+    $this->assertEquals('127.0.0.1', server::get('HTTP_HOST'));
+    $this->assertEquals('127.0.0.1', server::get('SERVER_NAME'));
+
     $_SERVER['SERVER_PORT'] = '<script>alert("xss")</script>999';
 
     $this->assertEquals('999', server::get('SERVER_PORT'));
