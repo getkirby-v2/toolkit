1fd8d986395a267e431b3daf813105c353c690ef
fanningert <thomas@fanninger.at>
2014-12-27 16:38:12 +0100

    Add mimetype for the generic file extension "text/plain"

--- lib/f.php
+++ lib/f.php
@@ -87,6 +87,7 @@ class F {
     'html'  => 'text/html',
     'htm'   => 'text/html',
     'shtml' => 'text/html',
+    'code'  => 'text/html',
     'txt'   => 'text/plain',
     'text'  => 'text/plain',
     'log'   => array('text/plain', 'text/x-log'),
