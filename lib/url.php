a7ddbca63d376be4edb9200aa084aef4e85336a5
Bastian Allgeier <mail@bastianallgeier.com>
2015-05-02 15:52:14 +0200

    Fix #82 and #80

--- lib/url.php
+++ lib/url.php
@@ -307,7 +307,7 @@ class Url {
     if(is_null($url)) {
       $port = server::get('SERVER_PORT');
       $port = in_array($port, array(80, 443)) ? null : $port;
-      return static::scheme() . '://' . server::get('SERVER_NAME') . r($port, ':' . $port);
+      return static::scheme() . '://' . server::get('SERVER_NAME', server::get('SERVER_ADDR')) . r($port, ':' . $port);
     } else {
       $port   = static::port($url);
       $scheme = static::scheme($url);
