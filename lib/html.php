9ad8d263f2b5d6a59a8355ba5fd94a974292aa3f
peterkelm <info@peterkelm.com>
2015-12-08 21:36:54 +0100

    fix to the email tag improvement
    strstr returns false if the "needle" isn't found in the string - not quite what we want.

--- lib/html.php
+++ lib/html.php
@@ -219,7 +219,7 @@ class Html {
    */
   public static function email($email, $text = null, $attr = array()) {
     /* show only the eMail address without additional parameters (if the 'text' argument is empty) */
-    if(empty($text)) $text = str::encode(strstr($email, '?', true));
+    if(empty($text)) $text = str::encode(current(explode('?', $email)));
     $email = str::encode($email);
     $attr  = array_merge(array('href' => 'mailto:' . $email), $attr);
     return static::tag('a', $text, $attr);
